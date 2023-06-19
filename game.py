33b5f6294192767b00b149caec23a0f26434feea
Jose Arenado <vidalstat@gmail.com>
2023-06-19 17:51:13 -0400

    feat: clean up files

--- game.py
+++ game.py
@@ -1,20 +1,9 @@
 import pygame, sys
 from settings import *
+from Player import *
 
 bg_image = pygame.image.load('assets/img/clouds.png')
 
-class Player(pygame.sprite.Sprite):
-    def __init__(self):
-        super().__init__()
-        self.image = pygame.image.load('assets/img/Woodcutter.png')
-        self.rect = self.image.get_rect()
-        self.rect.center = (WIDTH / 2, HEIGHT / 2)
-        pygame.draw.rect(self.image, red, self.rect, 3)
-
-    def update(self):
-        self.rect.x += 5
-        if self.rect.left > WIDTH:
-            self.rect.right = 0
 
 player = Player()
 
